69268e56e8da2e7c06cad36d56953101775854f3
Luiz Albano <luiz@tekne.eti.br>
2023-04-26 08:34:50 -0300

    Attendees page

--- src/Controller/AttendeeController.php
+++ src/Controller/AttendeeController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\Code;
+use App\Entity\Lecture;
+use App\Repository\CodeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ManagerRegistry;
 
+#[Route('/attendee')]
 class AttendeeController extends AbstractController
 {
-    #[Route('/attendee', name: 'app_attendee')]
-    public function index(): Response
+    #[Route('/validate/{hash}', name: 'app_attendee_validate', methods: ['GET'])]
+    public function index(Request $request, CodeRepository $codeRepository, ManagerRegistry $doctrine): Response
     {
+        $code = $codeRepository->findOneBy(['hash' => $request->get('hash')]);
         return $this->render('attendee/index.html.twig', [
-            'controller_name' => 'AttendeeController',
+            'code' => $code,
         ]);
     }
 }
